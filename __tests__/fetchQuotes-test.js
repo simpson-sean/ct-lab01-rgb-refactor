e60025c55cff51eda239a241cf0e4599451300a7
Sean <simpson.sean@outlook.com>
2021-07-20 13:13:23 -0700

    WORKDONE: Passing tests with munged data

--- __tests__/fetchQuotes-test.js
+++ __tests__/fetchQuotes-test.js
@@ -9,6 +9,6 @@ describe('fetchQuotes', () => {
     it('fetches quotes from a futurama API and presents specific values', async () => {
         const quotes = await mungedFuturamaQuotes();
         
-        expect(quotes).toEqual({ character:expect.any(String), quote:expect.any(String), image:expect.any(String)});
+        expect(quotes).toEqual({ name:expect.any(String), text:expect.any(String), image:expect.any(String)});
     })
 })
--- functions/fetchQuotes.js
+++ functions/fetchQuotes.js
@@ -4,16 +4,17 @@ import fetch from 'node-fetch';
 const getFuturamaQuotes = async () => {
     const res = await fetch('http://futuramaapi.herokuapp.com/api/quotes');
     const body = await res.json();
-    // const quotes = body[0];
-    // const mungeQuotes = quotes.map(quote => {
-    //     return {
-    //         name: quote.character,
-    //         text: quote.quote,
-    //         image: quote.image,
-    //     }
-    // });
-    return body[0];
-    //return mungeQuotes;
+    const quotes = body;
+    const mungeQuotes = quotes.map(quote => {
+        return {
+            name: quote.character,
+            text: quote.quote,
+            image: quote.image,
+        }
+    });
+    console.log(body);
+    //return body[0];
+    return mungeQuotes[0];
     
 }
 
