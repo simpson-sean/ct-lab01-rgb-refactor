850b496c469c4e645f400db2e00a0652c9fd51d9
Sean <simpson.sean@outlook.com>
2021-07-19 17:08:06 -0700

    WORKDONE: Moved files OUT of github folder

--- functions/getName.js
+++ functions/getName.js
@@ -1,11 +1,11 @@
 // Create a function that returns the 'name' property of an object
 
 
-function getName() {
+function getName(booger) {
     //Declares myDog and sets object data
-    const myDog = { name: 'Parker', age: 4, weight: '65 lbs', breed: 'Blue Healer' };
+    //const myDog = { name: 'Parker', age: 4, weight: '65 lbs', breed: 'Blue Healer' };
     //Returns targeted value from myDog object 
-    return myDog.name;
+    return booger.name;
 
 };
 
