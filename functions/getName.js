75cdf0f6216de96018a63afdab1cce5057c16b2d
Sean <simpson.sean@outlook.com>
2021-07-19 17:30:03 -0700

    WORKDONE: Tests Passing in BracketNotation

--- functions/getName.js
+++ functions/getName.js
@@ -1,7 +1,7 @@
 // Create a function that returns the 'name' property of an object
 function getName(booger) {
     //Returns NAME value of an object 
-    return booger.name;
+    return booger['name'];
 };
 
 export default getName;
